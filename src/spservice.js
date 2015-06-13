efc1f216447a3f0adc4752a79afac438a2273ea9
vishnurs <rsvishnuu@gmail.com>
2015-06-14 00:13:08 +0530

    test file added

--- src/spservice.js
+++ src/spservice.js
@@ -1,10 +1,8 @@
 angular.module('SharePointService', [])
 .service('SPService', ['$http', function ($http) {
     this.addListItem = function (data, listName) {
-        var params = data;
-        params.__metadata = {};
-        params.__metadata.type = 'SP.Data.'+listName+'ListItem';
-
+        data.__metadata = {};
+        data.__metadata.type = 'SP.Data.'+listName+'ListItem';
         var req = {
             method: 'POST',
             url: SP_GLOBAL.WEB_URL + "/_api/web/lists/getbytitle('"+listName+"')/items",
@@ -13,18 +11,14 @@ angular.module('SharePointService', [])
                 'Accept': 'application/json;odata=verbose',
                 'X-RequestDigest': SP_GLOBAL.REQUEST_DIGEST
             },
-            data: JSON.stringify(params),
+            data: JSON.stringify(data),
         };
-
         return $http(req);
     };
 
     this.updateListItem = function(data, metadata) {
-
-        var params = data;
-        params.__metadata = {};
-        params.__metadata.type = metadata.type;
-
+        data.__metadata = {};
+        data.__metadata.type = metadata.type;
         return $http({
             method: 'POST',
             url: SP_GLOBAL.WEB_URL + '/_api/' + metadata.id,
@@ -35,7 +29,7 @@ angular.module('SharePointService', [])
                 'X-HTTP-Method': 'MERGE',
                 'If-Match': metadata.etag
             },
-            data: JSON.stringify(params),
+            data: JSON.stringify(data),
         });
     }
 
@@ -64,13 +58,11 @@ angular.module('SharePointService', [])
         }
         var filterString = '';
         if(filters && filters instanceof Object ) {
-
             for(var columnName in filters) {
                 for(var symbol in filters[columnName]) {
                     filterString += columnName + ' ' + symbol + ' \'' + filters[columnName][symbol]+'\' and ';
                 }
             }
-
             filterString = filterString.substring(0, filterString.length-4);
             if(filterString) {
                 filterString = '$filter=' + filterString + '&';
@@ -81,11 +73,10 @@ angular.module('SharePointService', [])
         if(top) {
             topString = '$top=' + top;
         }
-        return $http.get(SP_GLOBAL.WEB_URL + "/_api/web/lists/getByTitle('"+SP_GLOBAL.LISTS[listName]+"')/items?" + selectString + filterString + topString);
+        return $http.get(SP_GLOBAL.WEB_URL + "/_api/web/lists/getByTitle('"+listName+"')/items?" + selectString + filterString + topString);
     }
 }])
 .constant( 'SP_GLOBAL', {
-    WEB_URL : 'XXXXX',
     WEB_URL : 'XXXXX',
     REQUEST_DIGEST : document.getElementById( '__REQUESTDIGEST' ).value
 });
