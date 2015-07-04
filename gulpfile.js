341127a2748918b51bf0a6dc32c86aebf936983d
vishnurs <rsvishnuu@gmail.com>
2015-07-04 23:15:27 +0530

    Add jshint gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
+'use strict';
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglifyjs');
+var jshint = require('gulp-jshint');
 
 gulp.task('default', function() {
     gulp.src('src/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
-})
+});
+
+gulp.task('vet', function() {
+    gulp.src('src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'), {verbose: true})
+        .pipe(jshint.reporter('fail'));
+});
+
